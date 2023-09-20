d7e879d2110186b81bcb66ecf0bf631813be3267
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-09-21 02:23:17 +0300

    refactor: удалить старые DTO объекты и методы

--- src/main/java/ru/sber/SberCoffee/service/impl/ItemServiceImpl.java
+++ src/main/java/ru/sber/SberCoffee/service/impl/ItemServiceImpl.java
@@ -2,11 +2,7 @@ package ru.sber.SberCoffee.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.sber.SberCoffee.dto.ItemDTO;
 import ru.sber.SberCoffee.entity.Item;
-import ru.sber.SberCoffee.entity.ItemPrice;
-import ru.sber.SberCoffee.entity.Size;
-import ru.sber.SberCoffee.repository.ItemPriceRepo;
 import ru.sber.SberCoffee.repository.ItemRepo;
 import ru.sber.SberCoffee.service.ItemService;
 
@@ -18,7 +14,6 @@ import java.util.List;
 public class ItemServiceImpl implements ItemService {
 
     ItemRepo itemRepo;
-    ItemPriceRepo itemPrice;
 
     @Override
     public List<Item> getItems() {
@@ -30,54 +25,9 @@ public class ItemServiceImpl implements ItemService {
         return itemRepo.findByName(name);
     }
 
-    public ItemDTO getItemDTOByName(String itemName) {
-        Item item = itemRepo.findByName(itemName);
-        if (item == null) {
-            return null; // Обработка ошибки, если товар не найден
-        }
-
-        return createItemDTO(item);
-    }
-
-
-    /////////
     @Override
     public void save(Item item) {
         itemRepo.save(item);
     }
-
-    public List<ItemDTO> getAllItems() {
-        List<Item> items = itemRepo.findAll();
-        List<ItemDTO> itemDTOs = new ArrayList<>();
-
-        for (Item item : items) {
-            itemDTOs.add(createItemDTO(item));
-        }
-
-        return itemDTOs;
-    }
-
-    private ItemDTO createItemDTO(Item item) {
-        List<ItemPrice> itemPrices = itemPrice.findItemPriceByItem(item);
-
-        ItemDTO itemDTO = new ItemDTO();
-        itemDTO.setName(item.getName());
-        itemDTO.setItem_id(item.getId());
-        itemDTO.setBasePrice((int) item.getBasePrice());
-
-        List<Long> sizeIds = new ArrayList<>();
-        List<Long> prices = new ArrayList<>();
-        for (ItemPrice itemPrice : itemPrices) {
-            Size size = itemPrice.getSize();
-            sizeIds.add((long) size.getId());
-            prices.add((long) size.getPriceModifier());
-        }
-
-        itemDTO.setSizes(sizeIds);
-        itemDTO.setModifiers(prices);
-
-        return itemDTO;
-    }
-
 
 }
