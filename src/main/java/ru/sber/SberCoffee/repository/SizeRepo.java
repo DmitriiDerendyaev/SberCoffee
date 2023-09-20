f5ae676d7712f22167b30fbb8d921a722532c9c1
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-09-21 00:01:48 +0300

    delete: удалить сущности прошлой версии БД

--- src/main/java/ru/sber/SberCoffee/repository/SizeRepo.java
+++
@@ -1,8 +0,0 @@
-package ru.sber.SberCoffee.repository;
-
-import jakarta.persistence.criteria.CriteriaBuilder;
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.sber.SberCoffee.entity.Size;
-
-public interface SizeRepo extends JpaRepository<Size, Integer> {
-}
