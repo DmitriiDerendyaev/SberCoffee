c3906e508df5cadd3348b8309d604143faf5022c
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-09-22 09:27:47 +0300

    feature: добавить валидацию полей Item и настроить логирование его контроллера

--- src/main/java/ru/sber/SberCoffee/controller/ItemController.java
+++ src/main/java/ru/sber/SberCoffee/controller/ItemController.java
@@ -1,6 +1,8 @@
 package ru.sber.SberCoffee.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.sber.SberCoffee.entity.Item;
@@ -9,6 +11,7 @@ import ru.sber.SberCoffee.service.ItemService;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @RestController
 @RequestMapping("/item")
 @RequiredArgsConstructor
@@ -20,9 +23,11 @@ public class ItemController {
         public ResponseEntity<List<Item>> getAllItems() {
                 List<Item> itemList = itemService.getAllItems();
                 if (itemList.isEmpty()) {
+                        log.warn("List of items is empty");
                         return ResponseEntity.notFound().build();
                 }
 
+                log.info("Retrieved {} items successfully", itemList.size());
                 return ResponseEntity.ok(itemList);
         }
 
@@ -31,8 +36,10 @@ public class ItemController {
                 Optional<Item> itemOptional = itemService.getItemById(id);
 
                 if (itemOptional.isPresent()) {
+                        log.info("Retrieved item with ID {}", id);
                         return ResponseEntity.ok(itemOptional.get());
                 } else {
+                        log.warn("Item with ID {} not found", id);
                         return ResponseEntity.notFound().build();
                 }
         }
@@ -42,28 +49,35 @@ public class ItemController {
                 Item item = itemService.getItemByName(name);
 
                 if (item != null) {
+                        log.info("Retrieved item by name: {}", name);
                         return ResponseEntity.ok(item);
                 } else {
+                        log.warn("Item with name '{}' not found", name);
                         return ResponseEntity.notFound().build();
                 }
         }
 
         @PostMapping
-        public ResponseEntity<Item> createItem(@RequestBody Item item) {
+        public ResponseEntity<Item> createItem(@Valid @RequestBody Item item) {
                 if (item == null) {
+                        log.warn("Received request with null item");
                         return ResponseEntity.badRequest().build();
                 }
 
-                return ResponseEntity.ok(itemService.createItem(item));
+                Item createdItem = itemService.createItem(item);
+                log.info("Created item with ID {}", createdItem.getId());
+                return ResponseEntity.ok(createdItem);
         }
 
         @PutMapping("/{id}")
-        public ResponseEntity<Item> updateItem(@PathVariable Long id, @RequestBody Item item) {
+        public ResponseEntity<Item> updateItem(@PathVariable Long id, @Valid @RequestBody Item item) {
                 Item updatedItem = itemService.updateItem(id, item);
 
                 if (updatedItem != null) {
+                        log.info("Updated item with ID {}", id);
                         return ResponseEntity.ok(updatedItem);
                 } else {
+                        log.warn("Item with ID {} not found for update", id);
                         return ResponseEntity.notFound().build();
                 }
         }
@@ -71,8 +85,10 @@ public class ItemController {
         @DeleteMapping("/{id}")
         public ResponseEntity<Void> deleteItem(@PathVariable Long id) {
                 if (itemService.deleteItem(id)) {
+                        log.info("Deleted item with ID {}", id);
                         return ResponseEntity.noContent().build();
                 } else {
+                        log.warn("Item with ID {} not found for deletion", id);
                         return ResponseEntity.notFound().build();
                 }
         }
--- src/main/java/ru/sber/SberCoffee/entity/Item.java
+++ src/main/java/ru/sber/SberCoffee/entity/Item.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,7 +18,9 @@ public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @NotBlank(message = "Name cannot be blank")
     private String name;
+    @Positive(message = "Position ID must be a positive number")
     private double price;
 
 }
