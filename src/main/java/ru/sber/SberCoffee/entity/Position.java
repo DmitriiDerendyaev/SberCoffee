7c2fb7643673c344424309459fdee7475a11a3c1
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-09-22 13:26:49 +0300

    refactor: добавить конструктор для NotNull

--- src/main/java/ru/sber/SberCoffee/entity/Position.java
+++ src/main/java/ru/sber/SberCoffee/entity/Position.java
@@ -8,15 +8,23 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Position {
+
+    public Position(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     @NotBlank(message = "Position name is required")
     private String name;
     private String description;
