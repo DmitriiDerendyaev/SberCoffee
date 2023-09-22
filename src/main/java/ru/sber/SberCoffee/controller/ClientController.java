a06d8d3906e57608cb026690e36fc2ce32b3e23f
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-09-22 09:38:49 +0300

    feature: добавить валидацию полей Client и настроить логирование его контроллера

--- src/main/java/ru/sber/SberCoffee/controller/ClientController.java
+++ src/main/java/ru/sber/SberCoffee/controller/ClientController.java
@@ -1,6 +1,8 @@
 package ru.sber.SberCoffee.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.sber.SberCoffee.entity.Client;
@@ -9,6 +11,7 @@ import ru.sber.SberCoffee.service.ClientService;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @RestController
 @RequestMapping("/client")
 @RequiredArgsConstructor
@@ -20,9 +23,11 @@ public class ClientController {
     public ResponseEntity<List<Client>> getAllClient(){
         List<Client> clientList = clientService.getAllClient();
         if(clientList.isEmpty()){
+            log.warn("No clients found");
             return ResponseEntity.notFound().build();
         }
 
+        log.info("Returned {} clients", clientList.size());
         return ResponseEntity.ok(clientList);
     }
 
@@ -31,37 +36,50 @@ public class ClientController {
         Optional<Client> clientOptional = clientService.getClientById(id);
 
         if(clientOptional.isPresent()){
+            log.info("Client with ID {} found", id);
             return ResponseEntity.ok(clientOptional.get());
         } else {
+            log.warn("Client with ID {} not found", id);
             return ResponseEntity.notFound().build();
         }
     }
 
     @PostMapping
-    public ResponseEntity<Client> createClient(@RequestBody Client client){
+    public ResponseEntity<Client> createClient(@Valid @RequestBody Client client){
         if(client == null){
+            log.warn("Received a null client request");
             return ResponseEntity.badRequest().build();
         }
 
-        return ResponseEntity.ok(clientService.createClient(client));
+        Client createdClient = clientService.createClient(client);
+        log.info("Created client with ID {}", createdClient.getId());
+        return ResponseEntity.ok(createdClient);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Client> updateClient(@PathVariable Long id, @RequestBody Client client){
-        Client updatedClient = clientService.updateClient(id, client);
+    public ResponseEntity<Client> updateClient(@PathVariable Long id, @Valid @RequestBody Client client){
+        if (client == null) {
+            log.warn("Received a null client update request");
+            return ResponseEntity.badRequest().build();
+        }
 
-        if(updatedClient != null){
+        Client updatedClient = clientService.updateClient(id, client);
+        if (updatedClient != null) {
+            log.info("Updated client with ID {}", id);
             return ResponseEntity.ok(updatedClient);
         } else {
+            log.warn("Client with ID {} not found", id);
             return ResponseEntity.notFound().build();
         }
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Client> deleteClient(@PathVariable Long id){
-        if(clientService.deleteClient(id)){
+        if (clientService.deleteClient(id)) {
+            log.info("Deleted client with ID {}", id);
             return ResponseEntity.noContent().build();
         } else {
+            log.warn("Client with ID {} not found", id);
             return ResponseEntity.notFound().build();
         }
     }
--- src/main/java/ru/sber/SberCoffee/entity/Client.java
+++ src/main/java/ru/sber/SberCoffee/entity/Client.java
@@ -1,6 +1,9 @@
 package ru.sber.SberCoffee.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,11 +18,24 @@ public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @NotBlank(message = "Surname is required")
     private String surname;
+
+    @NotBlank(message = "Patronymic is required")
     private String patronymic;
+
+    @NotBlank(message = "Phone number is required")
+    @Pattern(regexp = "\\d{11}", message = "Phone number should be 10 digits")
     private String phoneNumber;
+
+    @NotBlank(message = "Address is required")
     private String address;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
     @Column(name = "birthday")
     private LocalDate birthday;
