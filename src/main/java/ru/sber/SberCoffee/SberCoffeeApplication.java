9f7372aabb91a75e688279276d4a3eb9280f240d
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-09-26 14:40:18 +0300

    documentation: added Swagger

--- src/main/java/ru/sber/SberCoffee/SberCoffeeApplication.java
+++ src/main/java/ru/sber/SberCoffee/SberCoffeeApplication.java
@@ -4,8 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.ApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 /**
  * The type Sber coffee application.
@@ -26,5 +26,6 @@ public class SberCoffeeApplication {
 		SpringApplication.run(SberCoffeeApplication.class, args);
 
 	}
+
 
 }
